de6f4f3949cea193eea4cb9d987e71af57e4e3f2
Subho-bcrec <subho.prp@gmail.com>
2013-09-05 15:25:15 +0530

    Chat UI updated

--- kickchat/settings.py
+++ kickchat/settings.py
@@ -17,8 +17,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'kickchat',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'subho',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
