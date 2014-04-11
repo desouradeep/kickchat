cd104697a8adfb98f8acd7c006775eddc541206b
Souradeep De <souradeep.2011@gmail.com>
2014-04-11 20:47:38 +0530

    profiles: use get_object_or_404 instead of get

--- kickchat/apps/profiles/views.py
+++ kickchat/apps/profiles/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
-from django.shortcuts import render, render_to_response, redirect
+from django.shortcuts import (render, render_to_response, redirect,
+                              get_object_or_404)
 from django.template import Context, RequestContext
+
 
 def index(request, *args, **kwargs):
     if 'username' in kwargs:
@@ -10,17 +12,20 @@ def index(request, *args, **kwargs):
     else:
         user = request.user.username
 
-    user_data = User.objects.get(username=user)
+    user_data = get_object_or_404(User, username=user)
 
     user_details = {
-        'username' : user_data.username,
-        'fullname' : user_data.first_name + user_data.last_name,
-        'email' : user_data.email,
+        'username': user_data.username,
+        'fullname': user_data.first_name + ' ' + user_data.last_name,
+        'email': user_data.email,
     }
 
     context = RequestContext(request, {
-            'activate_navbar_element_id' : 'profile',
-            'user_details' : user_details,
-        })
-    return render_to_response('profiles/profiles.html', context_instance=context)
+        'activate_navbar_element_id': 'profile',
+        'user_details': user_details,
+    })
 
+    return render_to_response(
+        'profiles/profiles.html',
+        context_instance=context
+    )
